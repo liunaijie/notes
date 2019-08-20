3bb0e44733f180fc8dfac89d6ce9de6baa37040d
liunaijie <liunaijie1996@gmail.com>
2019-08-20 17:11:53 +0800

    add T2 T20 T28  T852

---
+++ code/LeetCode/src/T852.java
@@ -0,0 +1,49 @@
+/**
+ * @author LiuNaiJie
+ * on 2019-07-18
+ */
+public class T852 {
+
+	/**
+	 * 山脉数组的峰顶索引
+	 * 我们把符合下列属性的数组 A 称作山脉：
+	 * <p>
+	 * A.length >= 3
+	 * 存在 0 < i < A.length - 1 使得A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+	 * 给定一个确定为山脉的数组，返回任何满足 A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1] 的 i 的值。
+	 * <p>
+	 * 示例 1：
+	 * <p>
+	 * 输入：[0,1,0]
+	 * 输出：1
+	 * 示例 2：
+	 * <p>
+	 * 输入：[0,2,1,0]
+	 * 输出：1
+	 * 提示：
+	 * <p>
+	 * 3 <= A.length <= 10000
+	 * 0 <= A[i] <= 10^6
+	 * A 是如上定义的山脉
+	 */
+
+	/**
+	 * 前后寻找最大值，返回该坐标
+	 */
+	class Solution {
+		public int peakIndexInMountainArray(int[] A) {
+			int start = 0;
+			int end = A.length - 1;
+			while (start < end) {
+				if (A[start] < A[start + 1]) {
+					start++;
+				}
+				if (A[end] < A[end - 1]) {
+					end--;
+				}
+			}
+			return start;
+		}
+	}
+
+}
