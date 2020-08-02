be9068d2e3e0f4d6e4b42072004625c3b1ccc98f
LiuNaiJie <liunaijie1996@163.com>
2020-08-02 18:22:34 +0800

    add T226

---
+++ LeetCode/src/T226.java
@@ -0,0 +1,38 @@
+/**
+ * @author ：LiuNaiJie
+ * @date ：2020-03-21
+ */
+public class T226 {
+
+	/**
+	 * 翻转一棵二叉树。
+	 *
+	 * 示例：
+	 *
+	 * 输入：
+	 *
+	 *      4
+	 *    /   \
+	 *   2     7
+	 *  / \   / \
+	 * 1   3 6   9
+	 * 输出：
+	 *
+	 *      4
+	 *    /   \
+	 *   7     2
+	 *  / \   / \
+	 * 9   6 3   1
+	 */
+
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode right = invertTree(root.right);
+		TreeNode left = invertTree(root.left);
+		root.left = right;
+		root.right = left;
+		return root;
+	}
+}
